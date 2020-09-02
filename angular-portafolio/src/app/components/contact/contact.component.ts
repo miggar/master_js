4117d3dcce1bed0584525ab1854584a81fe9da82
miggar <miggar08@ucm.es>
2020-09-02 02:22:28 +0200

    angular-portafolio: slider como componente hijo

--- angular-portafolio/src/app/components/contact/contact.component.ts
+++ angular-portafolio/src/app/components/contact/contact.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-// no funciona: import * as $ from 'jquery';
-declare var $: any;
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  public widthSlider: number;
+  public widthToSlider: number;
+  public caption: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    $('.galeria').bxSlider({
-      mode: 'fade',
-      captions: true,
-      slideWidth: 800
-    });
-
-    // El carrusel tapa la cabecera
-    $('header').css({ zIndex: 1 });
+  constructor() {
+    this.caption = true;
+    this.widthToSlider = 0;
   }
 
+  ngOnInit(): void {}
+
+  loadSlider(): void {
+    this.widthToSlider = this.widthSlider;
+  }
+
+  resetSlider(): void {
+    this.widthToSlider = 0;
+  }
 }
--- angular-portafolio/src/app/components/slider/slider.component.ts
+++ angular-portafolio/src/app/components/slider/slider.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// no funciona: import * as $ from 'jquery';
+declare var $: any;
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
+  @Input() anchura: number;
+  // tslint:disable-next-line: no-input-rename
+  @Input('etiquetas') captions: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.anchura = 0;
   }
 
+  ngOnInit(): void {
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: this.captions,
+      slideWidth: this.anchura,
+    });
+
+    // El carrusel tapa la cabecera
+    $('header').css({ zIndex: 1 });
+  }
 }
